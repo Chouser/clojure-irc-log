34941174ad0a83e2af5ecab46a4ba7c01c44d734
Chouser <chouser@n01se.net>
2008-07-21 12:22:06 -0400

    Use new url: clojure-log.n01se.net

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/chouser/proj/clojure-log/
 java -cp ../../build/clojure/clojure.jar clojure.lang.Script irc-log-split.clj
-rsync -u *.html *.js *.css *.clj n01se.net:web/chouser/clojure-log/
+rsync -ua * n01se.net:clojure-log.n01se.net/

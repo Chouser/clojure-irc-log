8bb8783a437494722bbb3b0b919034bf164bbb60
Chouser <chouser@n01se.net>
2010-10-06 17:34:58 -0400

    Add support for a list of nicks to filter on.

--- irc.js
+++ irc.js
@@ -1,19 +1,48 @@
 window.onload = function() {
+  var opts = {};
   var m = /^#(.*)-(\d+\/)?(.*)$/.exec( document.location.hash );
   if( m ) {
+    opts = {start: m[1], end: m[3]}
+  }
+  else {
+    var parts = document.location.hash.substr(1).split(/\//);
+    for( var i = 0; i < parts.length; ++i ) {
+      m = /^(.*?)=(.*)/.exec( parts[i] );
+      opts[m[1]] = m[2];
+    }
+  }
+
+  if( opts.nicks ) {
+    opts.nicks = "," + opts.nicks + ",";
+  }
+
+  if( opts.start || opts.nicks ) {
     var p = document.getElementsByTagName('p');
-    var hide = true;
-    var a, lastnick;
+    var hide = opts.start;
+    var a, lastnick, m, nick;
     for( var i = 0; i < p.length; ++i ) {
       a = p[i].getElementsByTagName('a')[0];
       lastnick = p[i].getElementsByTagName('b')[0] || lastnick;
-      if( a.name == m[1] ) {
+      if( opts.nicks ) {
+        nick = null;
+        if( lastnick.innerHTML == '* ' ) {
+          nick = p[i].getElementsByTagName('em')[0].innerHTML;
+        }
+        else {
+          m = /^(.*):\s*/.exec(lastnick.innerHTML);
+          nick = m && m[1];
+        }
+        if( nick && opts.nicks.indexOf(","+nick+",") == -1) {
+          p[i].style.display = 'none';
+        }
+      }
+      if( a.name == opts.start ) {
         hide = false;
         a.className = 'nh';
         p[i].insertBefore( lastnick, a.nextSibling );
       }
       if( hide ) { p[i].style.display = 'none'; }
-      if( a.name == m[3] ) { hide = true; }
+      if( a.name == opts.end ) { hide = true; }
     }
   }
 

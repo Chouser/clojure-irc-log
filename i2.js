602c5fc06a051ea4e063d1919f7dfa1d7b22588a
Chouser <chouser@n01se.net>
2011-01-30 18:28:59 -0500

    Rock solid headers on every page

--- i2.js
+++ i2.js
@@ -1,25 +1,43 @@
 function resize() {
   // JavaScript is running, so convert page from vertical scrolling to
   // horizontal paging:
-  $('#frame').addClass('js');
-  $('#columns').addClass('js');
+  $('#frame').addClass('paged');
+  $('#columns').addClass('paged');
   $(document.body).height($(window).height() - 20);
 
-  // Measure total width of the columns and make #wide exactly that big.
-  $('#wide').css('width', $('#frame')[0].scrollWidth + 'px');
+  // Insert initial per-column header and footer to establish their vertical
+  // size so the page_count can be calculated correctly
+  $('#columns').before('<div id="headrow"></div>');
+  var header = $('.header');
+  $('#headrow').append(header);
 
-  // Insert per-column headers and footers
-  $('#columns').before('<h1 id="headrow">Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header Header </h1>');
   $('#columns').after('<div id="footrow">footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer</div>');
+  $('#headrow').height($('#headrow').outerHeight());
   $('#columns').height(
       $('#frame').innerHeight()
       - $('#headrow').outerHeight()
       - $('#footrow').outerHeight());
+
+  // Measure widths
+  var doc_width = window.col_width = $('#frame')[0].scrollWidth;
+  var col_width = window.col_width = $('#frame').innerWidth() - 1; //not quite right
+  var page_count = Math.ceil(doc_width / col_width);
+
+  // Make #wide exactly as wide as all the columns it contains
+  // This not only sets up the container for headers and footers, but works
+  // around a rendering bug in chrome
+  $('#wide').css('width', doc_width + 'px');
+
+  // Insert per-column headers and footers
+  $('#headrow').addClass('paged');
+  for(var i = 2; i < page_count; ++i) {
+    $('#headrow').append(header.clone());
+  }
 }
 
 function scrollBy(p) {
+  var width = window.col_width;
   var frame = $('#frame')[0];
-  var width = $('#frame').innerWidth(); // not quite right
   frame.scrollLeft = (Math.floor(frame.scrollLeft / width) + p) * width;
 }
 

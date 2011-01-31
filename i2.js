882910f76a3aac00df170efdc93d50da792705b9
Chouser <chouser@n01se.net>
2011-01-31 11:59:04 -0500

    Add correct footers with page numbers and nav buttons

--- i2.js
+++ i2.js
@@ -14,15 +14,16 @@ function resize() {
   var page_head = $('.page_head').clone();
   $('.page_head').css('visibility', 'hidden');
 
-  $('#columns').after('<div id="footrow">footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer footer</div>');
+  $('#columns').after('<footer><table class="page_foot"><tr><td class="page_foot_left"><button onclick="scrollBy(-1)">&lt;&lt;&lt;</button></td><td class="page_foot_mid"></td><td class="page_foot_right"><button onclick="scrollBy(1)">&gt;&gt;&gt;</button></td></tr></table></footer>');
+  var page_foot = $('.page_foot');
   $('header').height($('header').outerHeight());
   $('#columns').height(
       $('#frame').innerHeight()
       - $('header').outerHeight()
-      - $('#footrow').outerHeight());
+      - $('footer').outerHeight());
 
   // Measure widths
-  var doc_width = window.col_width = $('#frame')[0].scrollWidth;
+  var doc_width = window.doc_width = $('#frame')[0].scrollWidth;
   var col_width = window.col_width = $('#frame').innerWidth() - 1; //not quite right
   var page_count = Math.ceil(doc_width / col_width);
 
@@ -34,20 +35,27 @@ function resize() {
   // Insert per-column headers and footers
   for(var i = 2; i < page_count; ++i) {
     $('header').append(page_head.clone());
+    $('footer').append(page_foot.clone());
   }
+
+  // Fix page numbers
+  $('.page_foot_mid').each(function(i, elem) {
+    elem.innerHTML = 'page ' + (i+1) + ' of ' + (page_count-1);
+  });
 }
 
 function scrollBy(p) {
   var width = window.col_width;
   var frame = $('#frame')[0];
-  frame.scrollLeft = (Math.floor(frame.scrollLeft / width) + p) * width;
+  var page_count = Math.ceil(window.doc_width / width);
+  frame.scrollLeft = Math.min(page_count-2, (Math.floor(frame.scrollLeft / width) + p)) * width;
 }
 
 $(document).keydown(function(e) {
-  if(e.which == 39 || e.which == 32) {
+  if(e.which == 39 || e.which == 32 || e.which == 34 || e.which == 40) {
     scrollBy(1);
   }
-  if(e.which == 37) {
+  if(e.which == 37 || e.which == 33 || e.which == 38) {
     scrollBy(-1);
   }
 });
